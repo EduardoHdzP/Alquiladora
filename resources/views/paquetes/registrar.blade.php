c1173dc01e2fb03c56664ecec2fe8ee634c2e698
Eduardo <lalohernandez_0702@hotmail.com>
2018-12-01 21:37:00 -0600

    Algunos cambios
    Pequeños cambios

--- resources/views/paquetes/registrar.blade.php
+++ resources/views/paquetes/registrar.blade.php
@@ -1,6 +1,7 @@
 
 @include('encabezado')
 @include('menu')
+<?php $paquetes=null;?>
 
 <div class="modal fade" id="modalRegistro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -64,70 +65,46 @@
 </div>
 
 <div class="row mt-3">
-	<div class="col-12 col-sm-6 col-md-3" {{-- style="background: red;" --}}>
-		<div class="card m-0" style="width: 18rem;">
-		  <img class="card-img-top" src="{{ asset('img/sillas/silla.jpg') }}" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Silla Ghera</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary btn-block">Añadir</a>
-		  </div>
-		</div>
-	</div>
-	<div class="col-12 col-sm-6 col-md-3" {{-- style="background: green;" --}}>
-		<div class="card" style="width: 18rem;">
-		  <img class="card-img-top" src="{{ asset('img/sillas/silla.jpg') }}" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Silla Ghera</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary btn-block">Añadir</a>
-		  </div>
-		</div>
-	</div>
-	<div class="col-12 col-sm-6 col-md-3" {{-- style="background: yellow;" --}}>
-		<div class="card" style="width: 18rem;">
-		  <img class="card-img-top" src="{{ asset('img/mesas/mesa-redonda.jpg') }}" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Silla Ghera</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary btn-block">Añadir</a>
-		  </div>
-		</div>
-	</div>
-	<div class="col-12 col-sm-6 col-md-3" {{-- style="background: black;" --}}>
-		<div class="card" style="width: 18rem;">
-		  <img class="card-img-top" src="{{ asset('img/sillas/silla.jpg') }}" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Silla Ghera</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary btn-block">Añadir</a>
-		  </div>
-		</div>
-	</div>
-</div>
+	<div class="col-12 col-sm-12 col-md-12">
+		@if ($paquetes)
+			<div class="row">
+				@foreach ($paquetes as $p)
+					<div class="col-12 col-sm-6 col-md-3 mb-2" >
+						<div class="card" style="width: 18rem;">
+						  <img class="card-img-top imgCards" src="{{ asset('img/paquetes/'.$p->imagen.'') }}" alt="Card image cap">
+						  <div class="card-body">
+						  	<div class="row">
+						  		<div class="col-md-9"> <h5 class="card-title">{{ $p->nombre }}</h5></div>
+						  		<div class="col-md-3"><h5><span class="badge badge-success">{{ $p->ganancia }}%</span></h5></div>
+						  	</div>
+						    <p class="card-text m-0"><strong>Descripcion:</strong>
+						    	@if (strlen($p->descripcion)>54)
+							    	<?php echo substr($p->descripcion,0,52)."...";?>
+						    		
+						    	@else
+						    		{{$p->descripcion}}
+						    	@endif
+						    </p>
+						    <p class="card-text m-0"><strong>Medidas:</strong>{{ $p->medidas }}</p>
+						    <p class="card-text m-0"><strong>Utilidad:</strong>{{ $p->ganancia }}</p>
 
-
-<div class="row mt-3">
-	<div class="col-12 col-sm-6 col-md-3" {{-- style="background: red;" --}}>
-		<div class="card m-0" style="width: 18rem;">
-		  <img class="card-img-top" src="{{ asset('img/mesas/tablon-plegable-plastico.jpg') }}" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Silla Ghera</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary btn-block">Añadir</a>
-		  </div>
-		</div>
-	</div>
-	<div class="col-12 col-sm-6 col-md-3" {{-- style="background: green;" --}}>
-		<div class="card" style="width: 18rem;">
-		  <img class="card-img-top" src="{{ asset('img/sillas/silla.jpg') }}" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Silla Ghera</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary btn-block">Añadir</a>
-		  </div>
-		</div>
-	</div>
+						    <center>
+						    	<a onclick="detallesProducto('{{ $p->pro_id }}')" class="btn btn-info">Detalles</a>
+						    	<a href="/products/edit/{{ $p->pro_id }}" class="btn btn-warning">Editar</a>
+						    	<a onclick="eliminarProducto('{{ $p->pro_id }}')" href="#" class="btn btn-danger">Eliminar</a>
+						    </center>
+						  </div>
+						</div>
+					</div>
+				@endforeach
+			</div>
+		@else
+			<div class="alert alert-danger text-center">
+				<h1>No hay paquetes registrados</h1>
+			</div>
+		@endif
+	</div> 
+	
 </div>
 
 @include('pie');
